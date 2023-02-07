b0510d25056a359944962ca9437f6fb39e1c5052
Dragneel0112 <pillayruven@gmail.com>
2023-02-07 12:22:29 +0000

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student by
+"""
+
+
+class Student:
+    """
+    Class student and name last name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        """initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """return a dictionary"""
+        return self.__dict__
