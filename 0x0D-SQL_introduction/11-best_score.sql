fa299c21220c34952f3e9f9556cc8e1e070582f3
Dragneel0112 <pillayruven@gmail.com>
2023-03-14 12:29:43 +0000

    11-best_score.sql

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- Lists all rows of the table second_table
+-- Results display score and name
+-- ordered by score with score >= 10
+-- Database passed as argument in mysql
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
