0d6d5d9187f8829e52016c3ef12231a9b2609bcd
Dragneel0112 <pillayruven@gmail.com>
2023-03-14 12:10:47 +0000

    10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all rows of the table second_table
+-- Results display score and name and is ordered by score
+-- Database passed with argument in mysql
+SELECT score, name FROM second_table ORDER BY score DESC;
