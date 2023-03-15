f7d34b619074e90e016a378f94c27c5554e367eb
Dragneel0112 <pillayruven@gmail.com>
2023-03-15 12:48:35 +0000

    2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Creates database if not exists
+-- Creates user with select priviledges if not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
