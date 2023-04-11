4b7241d055efd71357b3732abcd3d0dd23ffc11e
Dragneel0112 <pillayruven@gmail.com>
2023-04-11 17:15:53 +0000

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Prints phrase x times, where x is arg
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
