91864f9d69e4ca89af34974f367df899b8d0d1c7
Dragneel0112 <pillayruven@gmail.com>
2023-04-11 17:41:12 +0000

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Prints a square
+const num = Math.floor(Number(process.argv[2]));
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    let x = '';
+    for (let j = 0; j < num; j++) x += 'X';
+    console.log(x);
+  }
+}
