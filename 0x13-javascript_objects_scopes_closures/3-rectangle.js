eccf074a253cc466ab12bc0c7eb2636916c29151
Dragneel0112 <pillayruven@gmail.com>
2023-04-13 14:23:35 +0000

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Creates class Rectangle
+class Rectangle {
+  // Instantiate Rectangle with height and width
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    // Method to print Rectangle using 'X'
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
