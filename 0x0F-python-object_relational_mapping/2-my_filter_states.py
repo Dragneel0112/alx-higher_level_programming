08f02086a905e1ecc1052594afa287fc4dd49a43
Dragneel0112 <pillayruven@gmail.com>
2023-04-15 15:17:47 +0000

    2-my_filter_states.py Edited Query removed ASC

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,9 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", charset="utf8", user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    state = argv[4]
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE %s ORDER BY states.id ASC """, (state,))
+    cur.execute("""SELECT * FROM states WHERE name LIKE %s""", (argv[4],))
     row = cur.fetchall()
     for r in row:
         print(r)
