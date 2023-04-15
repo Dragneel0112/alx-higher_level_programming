773f7b7eecfac8bed4a5d8889af3028874777865
Dragneel0112 <pillayruven@gmail.com>
2023-04-15 14:12:09 +0000

    Pycodestyle error fixed on Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", charset="utf8", user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
-    cur  = db.cursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM states")
     row = cur.fetchall()
     for r in row:
