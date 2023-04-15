a219ea300fc2f618dce41e2c0d209cae55cdaf8c
Dragneel0112 <pillayruven@gmail.com>
2023-04-15 15:39:16 +0000

    Testing new Script for Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Lists a state specified in args from the database"""
+""" Lists states from database where name matches argument"""
 
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states WHERE name LIKE %s
                    ORDER BY states.id ASC""", (sys.argv[4],))
