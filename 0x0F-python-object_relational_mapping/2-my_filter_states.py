df392b5e794f97a910ee67a93596d93bfec727de
Dragneel0112 <pillayruven@gmail.com>
2023-04-15 15:27:41 +0000

    Ammended file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """ Lists a state specified in args from the database"""
+
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                (argv[4],))
     row = cur.fetchall()
     for r in row:
         print(r)
