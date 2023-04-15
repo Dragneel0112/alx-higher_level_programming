e25478a6f0a75cb8ff4da3cc4058920f0170efa5
Dragneel0112 <pillayruven@gmail.com>
2023-04-15 15:22:36 +0000

    Edited Task 2 droped order by and made query shorter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,10 +5,10 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", charset="utf8", user=argv[1],
+    db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE %s""", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (argv[4],))
     row = cur.fetchall()
     for r in row:
         print(r)
