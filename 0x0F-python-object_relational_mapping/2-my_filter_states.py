42c8ce6a92d400c34ffc17adf61f5897a6e3accd
Dragneel0112 <pillayruven@gmail.com>
2023-04-15 15:49:50 +0000

    Edited format error in Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,17 +2,18 @@
 """ Lists states from database where name matches argument"""
 
 import MySQLdb
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name LIKE %s
-                   ORDER BY states.id ASC""", (sys.argv[4],))
-    row = cursor.fetchall()
+    db = MySQLdb.connect(host="localhost", charset="utf8", user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    row = cur.fetchall()
     for r in row:
-        print(r)
-    cursor.close()
+        if r[1] == argv[4]:
+            print(r)
+    cur.close()
     db.close()
