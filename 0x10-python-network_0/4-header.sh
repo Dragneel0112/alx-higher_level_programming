34e1ba9fda88c437453d34b5ba29bc41d06232ab
Dragneel0112 <pillayruven@gmail.com>
2023-04-28 16:54:29 +0000

    4-header.sh Edited

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Using curl to send GET request and diplay header response from server
-curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl "$1" -sX GET -H "X-HolbertonSchool-User-Id: 98"
