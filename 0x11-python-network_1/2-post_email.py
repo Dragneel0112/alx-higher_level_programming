9a7513a03cd29345ea8e4a927fdddc50ccf22035
Dragneel0112 <pillayruven@gmail.com>
2023-04-29 16:34:46 +0000

    Task 2 changed imports

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,15 +3,15 @@
 Using URLlib and sys to request url and post an email request
 """
 from sys import argv
-import urllib
+import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
     url = argv[1]
     value = {"email": argv[2]}
-    data = urllib.parse.urlencode(value)
-    data = data.encode("ascii")
+    data = urllib.parse.urlencode(value).encode("ascii")
 
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as res:
-        print(res.read().decode("ascii"))
+        print(res.read().decode("utf-8"))
